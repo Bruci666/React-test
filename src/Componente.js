d412c7c4e1331c3f5706b9277c14fcd3aa53cb0e
Bruci666 <silviobruciaferro9524@gmail.com>
2022-10-28 18:10:54 -0300

    primeiro commit

---
+++ src/Componente.js
@@ -0,0 +1,10 @@
+function Componente (){
+
+    return ( <>
+        <h1>HELLOW WORLRD</h1>
+        <h2>IHOSANA</h2>
+        <button>APERTE</button>
+      
+      </>);
+}
+export default Componente;
